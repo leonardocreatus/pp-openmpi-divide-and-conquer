bcb8ccbb3c9cd7d5e5b2aa554894837e1f63169b
Leonardo Barbosa <barbosa@Leonardos-MacBook-Pro.local>
2023-11-12 19:28:05 -0300

    feat: alloc array

--- main.cpp
+++ main.cpp
@@ -5,14 +5,8 @@
 #include "sort.hpp"
 #include "initialize-vector.hpp"
 
-#define DEBUG 1            // comentar esta linha quando for medir tempo
+// #define DEBUG 1            // comentar esta linha quando for medir tempo
 #define ARRAY_SIZE 1000000 // trabalho final com o valores 10.000, 100.000, 1.000.000
-
-/*
-   X
- X   X
-X X X X 
-*/
 
 int main(int argc, char* argv[]){
 
@@ -32,11 +26,13 @@ int main(int argc, char* argv[]){
     }
 
     int father_id = rank == 0 ? -1 : (rank - 1) / 2;
+    bool is_leaf = 2 * rank + 1 >= size;
     int left_child_id = 2 * rank + 1;
     int right_child_id = 2 * rank + 2;
     int size_vector;
     int *vector;
     
+    vector = new int[ARRAY_SIZE];
 
     if( rank != 0){
         MPI_Recv(vector, ARRAY_SIZE, MPI_INT, father_id, 0, MPI_COMM_WORLD, &status);
@@ -49,8 +45,10 @@ int main(int argc, char* argv[]){
     if(size_vector <= delta){
         sort(size_vector, vector); 
     }else {
+
         MPI_Send(&vector[0], size_vector/2, MPI_INT, left_child_id, 0, MPI_COMM_WORLD);
         MPI_Send(&vector[size_vector/2], size_vector/2, MPI_INT, right_child_id, 0, MPI_COMM_WORLD);
+
 
         MPI_Recv(&vector[0], size_vector/2, MPI_INT, left_child_id, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&vector[size_vector/2], size_vector/2, MPI_INT, right_child_id, 0, MPI_COMM_WORLD, &status);
@@ -63,17 +61,17 @@ int main(int argc, char* argv[]){
     }
 
     
-        if(rank == 0){
-            #ifdef DEBUG
-                printf("\nVetor: ");
-                for (int i=0 ; i< ARRAY_SIZE; i++){
-                    printf("[%03d] ", vector[i]);
-                }
-            #endif
-            auto end = std::chrono::high_resolution_clock::now();
-		    std::chrono::duration<double, std::milli> ms = end - start;
-		    std::cout << "time: " << ms.count() << "ms" << std::endl;
-        }
+    if(rank == 0){
+        #ifdef DEBUG
+            printf("\nVetor: ");
+            for (int i=0 ; i< ARRAY_SIZE; i++){
+                printf("[%03d] ", vector[i]);
+            }
+        #endif
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::milli> ms = end - start;
+        std::cout << "time: " << ms.count() << "ms" << std::endl;
+    }
     
 
     MPI_Finalize();
